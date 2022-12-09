e4b6fba058de3dd7f6d6490473bd70399f12674e
Xenon347 <104971531+Xenon347@users.noreply.github.com>
2022-12-09 20:35:44 +0400

    УДАР МОЛНИИ

--- Assets/Scripts/WizardController.cs
+++ Assets/Scripts/WizardController.cs
@@ -11,6 +11,13 @@ public class WizardController : MonoBehaviour
     public Transform leftController;
     public Transform rightController;
 
+    public AudioSource Eattack1;
+    public AudioSource Eattack2;
+    public AudioSource Fattack1;
+    public AudioSource Fattack2;
+    public AudioSource Fattack3;
+    public AudioSource Fattack4;
+
     //add health bar
     public HealthBar healthBar;
 
@@ -30,6 +37,9 @@ public class WizardController : MonoBehaviour
     public float maxAttackSpellCooldown = 1.0f;
     public float maxFirstSpellCooldown = 2.0f;
     public float maxSecondSpellCooldown;
+
+    public bool flag = true;
+    public byte flag2 = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -97,6 +107,20 @@ public class WizardController : MonoBehaviour
             Transform coorR = leftController.transform;
             //Transform coorR = gameObject.transform;
             Instantiate(attackSpellPrefab, new Vector3(coorR.position.x, coorR.position.y, coorR.position.z), coorR.transform.rotation);
+            if (flag2 == 0){
+                Fattack1.Play();
+            }else if (flag2 == 1)
+            {
+                Fattack2.Play();
+            }else if (flag2 == 2)
+                Fattack3.Play();
+            else
+            {
+                Fattack4.Play();
+                flag2 = 0;
+                return;
+            }
+            flag2++;
         }
     }
     public void CastFirstSpell()
@@ -106,6 +130,16 @@ public class WizardController : MonoBehaviour
             currentFirstSpellCooldown = maxFirstSpellCooldown;
             Transform coorL = rightController.transform;
             Instantiate(firstSpellPrefab, new Vector3(coorL.position.x, coorL.position.y, coorL.position.z), coorL.transform.rotation);
+            if (flag){
+                Eattack2.Play();
+                flag = false;
+            }
+            else
+            {
+                Eattack1.Play();
+                flag = true;
+            }
+            
         }
     }
 }
